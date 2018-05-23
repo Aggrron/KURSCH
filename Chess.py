8904db9d0f38b24d6bbf79f8b06edbf3cd6de1a6
Aggrron <gsm10forhl2@yandex.ru>
2018-05-23 18:27:42 +0300

    stp

--- Chess.py
+++ Chess.py
@@ -1,14 +1,44 @@
 import pygame
 
+class Figure:
+    def __init__(self, fig_x, fig_y, filename):
+        self.x = fig_x
+        self.y = fig_y
+        self.bitmap = pygame.image.load(filename)
+
+    def render(self):
+        gameDisplay.blit(self.bitmap, (self.x, self.y))
+
+
+
+
 pygame.init()
 #Параметры экрана
-display_w = 900
-display_h = 900
-
+display_w = 800
+display_h = 600
+#Цвета
+black = (0, 0, 0)
+grey = (100, 100, 100)
+white = (255, 255, 255)
+fancyBlue = (71, 71, 80)
 
 gameDisplay = pygame.display.set_mode((display_w, display_h))
 pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
+#Фигуры
+bp1 = Figure(100, 100, "black_pawn.png")
+
+#Параметры поля
+fieldImg = pygame.image.load('chess field.png')
+fieldSize = ((display_w*2)//3, (display_w*2)//3)
+fieldX = display_w/2 - fieldSize[0]/2
+fieldY = display_h/2 - fieldSize[1]/2
+fieldImg = pygame.transform.scale(fieldImg, fieldSize)
+
+
+def field(x, y):
+    gameDisplay.blit(fieldImg, (x, y))
+
 
 done = False
 
@@ -18,10 +48,13 @@ while not done:
         if e.type == pygame.QUIT:
             done = True
 
-        #print(e)
 
+    # Цвет и отрисовка
+    gameDisplay.fill(fancyBlue)
+    field(fieldX, fieldY)
+    bp1.render()
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(60)
 
 pygame.quit()
 quit()
